60cbe01dec9f76a5c1a9a2d79164512f74530d74
Raphael Beer <raphaelbeer030@gmail.com>
2015-07-17 16:56:34 +0200

    that should do

---
+++ library.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var AddACPMenu = {};
+
+AddACPMenu.addPluginMenu = function(header, callback) {
+    header.plugin_menu = [{
+            "icon": 'fa-location-arrow',
+            "route": 'link1.html',
+            "name": 'Link1'
+        },
+        {
+            'icon': 'fa-location-arrow',
+            'route': 'link2.html',
+            'name': 'Link2'
+        }
+    ];
+    callback(null, header);
+};
+module.exports = AddACPMenu;
